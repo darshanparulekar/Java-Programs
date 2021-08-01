6b72c66006ccf90b621d55a7ec6fad77fdf6cdcb
Darshan Parulekar <parulekardarshan29@gmail.com>
2021-08-01 11:18:50 +0530

    Hello World Program in Java
    Hello World Program in Java

---
+++ hello_world.java
@@ -0,0 +1,6 @@
+import java.io.*; //Input/Output Package
+class hello_world{ //class
+	public static void main(String args[]){ //main method with arguments array
+		System.out.println("Hello World"); 
+	}
+}
