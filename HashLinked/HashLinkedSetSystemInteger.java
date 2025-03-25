a7e58fafbbb4920aaa51dcc618fbdd17f3ee85b2
Darshan Parulekar <parulekardarshan29@gmail.com>
2025-03-25 13:30:40 +0530

    Create HashLinkedSetSystemInteger.java

---
+++ HashLinked/HashLinkedSetSystemInteger.java
@@ -0,0 +1,22 @@
+package org.HashLinkedSetSystemInteger;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HashLinkedSetSystemInteger {
+    public static void main(String[] args) {
+        // Creating a LinkedHashSet
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+
+        // Adding elements
+        linkedHashSet.add("Apple");
+        linkedHashSet.add("Banana");
+        linkedHashSet.add("Orange");
+        linkedHashSet.add("Grapes");
+        linkedHashSet.add("Banana"); // Duplicate, will not be added
+
+        // Displaying elements (Insertion order is maintained)
+        System.out.println("LinkedHashSet Elements: " + linkedHashSet);
+
+    }
+}
