8d58d478474c8e1a0b321e7931deb77810690635
Darshan Parulekar <parulekardarshan29@gmail.com>
2025-03-25 13:27:50 +0530

    Create HashLinkedSetUserString.java

---
+++ HashLinked/HashLinkedSetUserString.java
@@ -0,0 +1,36 @@
+package org.HashLinkedSetUserString;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HashLinkedSetUserString {
+    public static void main(String[] args) {
+        // Creating a LinkedHashSet
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking user input
+        System.out.println("Enter elements to add to the LinkedHashSet (type 'exit' to stop):");
+
+        while (true) {
+            System.out.print("Enter element: ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            boolean added = linkedHashSet.add(input);
+            if (!added) {
+                System.out.println("Duplicate entry! '" + input + "' is already in the set.");
+            }
+        }
+
+        // Closing scanner
+        scanner.close();
+
+        // Displaying elements (Insertion order is maintained)
+        System.out.println("\nLinkedHashSet Elements: " + linkedHashSet);
+    }
+}
